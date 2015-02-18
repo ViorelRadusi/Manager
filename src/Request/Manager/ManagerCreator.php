f5fe6dfdbe5363444a88a7bd13cf8641777d7000
Viorel RADUSI <vio@Dev-iMac-20-Radush.local>
2015-02-18 12:55:09 +0200

    changes base method form for to make.. for is keyword :(

--- src/Request/Manager/ManagerCreator.php
+++ src/Request/Manager/ManagerCreator.php
@@ -4,7 +4,7 @@ use Config, App;
 
 class ManagerCreator  {
 
-  public function for($managerClass){
+  public function make($managerClass){
 
     $class = Config::get('manager::mSpace') . $managerClass . "Manager";
     return App::make($class);
