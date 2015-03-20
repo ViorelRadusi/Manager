3a14fcb4928688a8058e64399f387ad5402c66a0
Viorel RADUSI <vio@Dev-iMac-20-Radush.local>
2015-03-20 13:29:47 +0200

    reverse params in interface

--- src/Request/Manager/Interfaces/ManagerInterface.php
+++ src/Request/Manager/Interfaces/ManagerInterface.php
@@ -4,7 +4,7 @@
     public function all();
     public function find($id);
     public function create(array $input);
-    public function update($id, array $input);
+    public function update(array $input, $id);
     public function delete($id);
     public function count();
   }
